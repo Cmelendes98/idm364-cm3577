e992faaad602eeb7715a38d85885335a92424a4e
Craig Melendes <32467837+Cmelendes98@users.noreply.github.com>
2019-06-09 20:47:01 -0700

    whole prooject upload

---
+++ App.js
@@ -0,0 +1,77 @@
+import './App.css';
+import React, {Component} from 'react';
+import { data } from './dataConstant';
+
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        items: data,
+        orderItems: [],
+      } // {data: data}
+    }
+   remove(event, target) {
+event.preventDefault();
+const newItems = this.state.items.filter(item => item.id !== target.id);
+this.setState({items: newItems})
+  }
+  add(newItem) {
+    const newOrderItems = [...this.state.orderItems, newItem];
+    const newItems = this.state.items.map(item => {
+      if (item.id === newItem.id) {
+        const newAvailability = item.availability-1;
+        return {...item, availability: newAvailability};
+      }
+      return item;
+    });
+    this.setState({ orderItems: newOrderItems, items: newItems });
+  }
+  render() {
+   const total = this.state.orderItems.reduce((total, {itemPrice}) => total += itemPrice, 0);
+    return (
+      <div>
+        <div id='menu'>
+        <img src="/logo/logo_copy.png" />
+        <u><h1> Menu </h1></u>
+          {this.state.items.map(item => {
+      	 if (!item.availability || item.availability == 0) {
+    		return (<div>
+              SOLD OUT, SORRY
+            </div>);
+    	} else {
+          return (<div>
+              <p key={item.id}><img src={'/imgs/'+item.imgName}/><strong>{item.itemName}</strong> ${item.itemPrice} <br/> {item.description}</p>
+
+              <button onClick={() => this.add(item)}>
+                      add
+                    </button>
+          </div>)}
+        })}
+        }
+
+    </div>
+<div id="order">
+    <u><h1> Order </h1></u>
+    {this.state.orderItems.map(item => (
+          <p key={item.id}><strong>{item.itemName}</strong> ${item.itemPrice}</p>
+          ))}
+    <h2> total: {total} </h2>
+</div>
+<div id="inventory">
+ <u><h1> Inventory </h1></u>
+        {this.state.items.map(item => (
+          <div>
+            <p key={item.id}>{item.itemName} ${item.itemPrice} {item.availability} <br />img: {item.imgName}</p>
+            <button onClick={(event) => this.remove(event,item)}>
+            Remove
+          </button>
+            </div>
+        ))}
+</div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ dataConstant.js
@@ -0,0 +1,130 @@
+export const data = [
+   {
+      "id": 1,
+      "itemName":"TastieGo Blueberry Yogart",
+      "imgName":"1.png",
+      "itemPrice":10,
+      "description":"Our chef has upped the Greek yogurt and granola game by adding a blueberry bombshell to this delicious mix-and-eat healthy snack. We've combined smooth, creamy nonfat Greek yogurt with blueberry purée to form a truly decadent base that's a good source of calcium. And we've thrown in a handful of fresh blueberries for some extra zing and vitamin C ",
+   	  "availability":10,
+   },
+   {
+      "id": 2,
+      "itemName":"Hudson Valley Kale and Mushroom Quiche",
+      "imgName":"2.png",
+      "itemPrice":10,
+      "description":"Made by skilled artisans, this earthy and tart quiche features quality local ingredients: eggs from Feather Ridge Farms and local milk, cheese, and butter. The tender, flaky crust is abundantly filled with sautéed spinach and just the right amount of savory custard.",
+      "availability":10,
+   },
+    {
+      "id": 3,
+      "itemName":"Yummy Blue Cheese Pizza",
+      "imgName":"3.png",
+      "itemPrice":10,
+      "description":"Earthy blue cheese, smoky bacon and sweet, sweet caramelized Vidalia onions come together in truly tasty fashion on a smoky, thin semolina crust crisped on the grill. When we first tasted this pizza, we thought it would pair perfectly with a crisp apple cider and lemony-dressed green salad.",
+   	  "availability":10,
+    },
+    {
+      "id": 4,
+      "itemName":"Mac and Cheese",
+      "imgName":"4.png",
+      "itemPrice":10,
+      "description":"Few can resist the charms of the classic mac & cheese, but our chefs take this comfort food to a new level of decadence with a crisp parmesan bread crumb topping holding back a creamy tide of nutty gruyere cheese melted around beautiful spirals of cavatappi pasta. Trust us... you won't have leftovers.",
+   	  "availability":10,
+    },
+    {
+      "id": 5,
+      "itemName":"cauliflower",
+      "imgName":"5.png",
+      "itemPrice":10,
+      "description":"What a delicious way to enjoy good-for-you cruciferous vegetables! This cauliflower dish picks up bold flavors from our chefs' Mediterranean-style combination of capers, lemon and parsley. It steams to perfection in just minutes, and makes a sophisticated side alongside grilled meats and salmon, balsamic-marinated chicken breast, lamb chops and provençal-style tuna.",
+   	  "availability":10,
+    },
+    {
+      "id":6,
+      "itemName":"baked chicken breast",
+      "imgName":"6.png",
+      "itemPrice":10,
+      "description":"Crumbled chicken breast combines with fresh kale and crunchy almonds to create a protein-packed salad. A tasty and original side for any main dish, it's also perfect in a pita or super in soups. Enjoy it as a post-workout meal or steal the show at your next potluck picnic.",
+   	  "availability":10,
+    },
+    {
+      "id": 7,
+      "itemName":"Meat Balls",
+      "imgName":"7.png",
+      "itemPrice":10,
+      "description":"Heavy on the ground beef and light on the bread crumbs, with garlic, onion, and parmesan cheese to give them extra flavor. The included tomato sauce has a concentrated tomato flavor with a pleasant sweetness — and we've made sure that it's enough to coat the pasta of your choice. Serve with garlic bread and a crisp green salad for a simple, timeless meal.",
+   	  "availability":10,
+    },
+    {
+      "id": 8,
+      "itemName":"Chicken Wings",
+      "imgName":"8.png",
+      "itemPrice":10,
+      "description":"Rich, meaty chicken thighs come to life with this addictive classic tikka marinade. Made with yogurt, garlic, ginger, lime juice and an assortment of warm spices — a blend so beloved in England, it was once claimed Britain's true national dish. Serve with our chana dahl for a royal feast.",
+   	  "availability":10,
+    },
+    {
+      "id": 9,
+      "itemName":"salmon fillet",
+      "imgName":"9.png",
+      "itemPrice":10,
+      "description":"Say hello to your new go-to salmon fillet. Tender and meaty, this roasted salmon is great served with roasted potatoes and our grilled asparagus. We recommend washing it down with a glass of crisp, dry white wine. The profound simplicity of this fillet means not only is it great on a plate, but also in a salad bowl.",
+   	  "availability":10,
+    },
+    {
+      "id": 10,
+      "itemName":"Turkey Burgers",
+      "imgName":"10.png",
+      "itemPrice":10,
+      "description":"We took our classic turkey burger and slathered it in tangy and sweet barbecue sauce to create a perfect patty for lunch or dinner. Delicious and satisfying, our burger comes fully cooked (just heat and eat!) and is great on its own or on a bun. If you're looking to go all out we recommend melting a slice of cheddar on top, throwing on a couple of bacon strips (yes, we went there), and topping with our handmade small-batch guacamole. Yum!",
+   	  "availability":10,
+    },
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ];
+
