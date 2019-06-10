30c2dc14acf1663e4201b7649ccf669120566022
Craig Melendes <32467837+Cmelendes98@users.noreply.github.com>
2019-06-10 08:10:33 -0700

    Build
    Here is the build folder that holds content can be found online

---
+++ build/precache-manifest.ae8a861502c82d382211904d253f884a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd161b64ed1225516656315655603c27",
+    "url": "index.html"
+  },
+  {
+    "revision": "8e1791728d505d2950fa",
+    "url": "static/css/main.9addbd1e.chunk.css"
+  },
+  {
+    "revision": "fb0324c4f6ed8e52ded5",
+    "url": "static/js/2.427a177d.chunk.js"
+  },
+  {
+    "revision": "8e1791728d505d2950fa",
+    "url": "static/js/main.dc5b9170.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "static/js/runtime~main.a8a9905a.js"
+  }
+]);
